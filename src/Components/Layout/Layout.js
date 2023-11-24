3737075eb70004210dd5d88b8bf8456df51aa4ae
kamalubd <u.kamal.88@gmail.com>
2023-11-24 11:01:04 +0600

    funtion done to compare correct answer

--- src/Components/Layout/Layout.js
+++ src/Components/Layout/Layout.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from '../Header/Header';
 import { Outlet } from 'react-router-dom';
+import QuizTopics from '../QuizTopics/QuizTopics';
 
 const Layout = () => {
     return (
         <div>
             <Header></Header>
             <Outlet></Outlet>
+            {/* <QuizTopics></QuizTopics> */}
             
         </div>
     );
--- src/Components/Question/Question.js
+++ src/Components/Question/Question.js
@@ -2,19 +2,34 @@ import React from 'react';
 import './Question.css'
 
 const Question = ({questions}) => {
-    console.log(questions);
+    const {question, options,correctAnswer} = questions;
 
-    const {question, options} = questions;
-    // console.log(options)
+    console.log(correctAnswer);
+
+    const valueByClick = (e) => {
+        const {value } = e.target;
+        console.log(e);
+
+        if (value === correctAnswer) {
+
+            alert('This is correct')
+            
+        }
+        else{
+            alert('This is not correct')
+        }
+      };
+    
     return (
         <div>
             <div className="question-container">
             <p>{question}</p>
             <div>
-                <p>{options[0]}</p>
-                <p>{options[1]}</p>
-                <p>{options[2]}</p>
-                <p>{options[3]}</p>
+                <p><input onChange={valueByClick} type="radio" value={options[0]} name="gender" /> {options[0]}</p>
+                <p><input onChange={valueByClick} type="radio" value={options[1]} name="gender" /> {options[1]}</p>
+                <p><input onChange={valueByClick} type="radio" value={options[2]} name="gender" /> {options[2]}</p>
+                <p><input onChange={valueByClick} type="radio" value={options[3]} name="gender" /> {options[3]}</p>
+
             </div>
             </div>
 
