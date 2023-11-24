41dcd530ea99037581c0a608af547da906a36ca2
kamalubd <u.kamal.88@gmail.com>
2023-11-24 12:23:01 +0600

    Toast notification done

--- src/Components/Question/Question.js
+++ src/Components/Question/Question.js
@@ -1,39 +1,54 @@
 import React from 'react';
-import './Question.css'
+import './Question.css';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
-const Question = ({questions}) => {
-    const {question, options,correctAnswer} = questions;
 
-    console.log(correctAnswer);
+const Question = ({ questions }) => {
+    const { question, options, correctAnswer } = questions;
+
+
+    console.log(questions);
 
     const valueByClick = (e) => {
-        const {value } = e.target;
-        console.log(e);
+        const { value } = e.target;
+        console.log(value);
 
         if (value === correctAnswer) {
 
-            alert('This is correct')
-            
+            toast('Bravo!! Your Answer is Correct!')
         }
-        else{
-            alert('This is not correct')
+
+        else {
+            toast('Oh!!! This Answer is Wrong. Try Again')
         }
-      };
-    
+    };
+
     return (
         <div>
+
             <div className="question-container">
-            <p>{question}</p>
-            <div>
-                <p><input onChange={valueByClick} type="radio" value={options[0]} name="gender" /> {options[0]}</p>
-                <p><input onChange={valueByClick} type="radio" value={options[1]} name="gender" /> {options[1]}</p>
-                <p><input onChange={valueByClick} type="radio" value={options[2]} name="gender" /> {options[2]}</p>
-                <p><input onChange={valueByClick} type="radio" value={options[3]} name="gender" /> {options[3]}</p>
-
+                <p>{question}</p>
+                <div>
+                    <p><input onClick={valueByClick} type="radio" value={options[0]} name="gender" /> {options[0]}</p>
+                    <p><input onClick={valueByClick} type="radio" value={options[1]} name="gender" /> {options[1]}</p>
+                    <p><input onClick={valueByClick} type="radio" value={options[2]} name="gender" /> {options[2]}</p>
+                    <p><input onClick={valueByClick} type="radio" value={options[3]} name="gender" /> {options[3]}</p>
+                </div>
+                <ToastContainer
+                    position="top-center"
+                    autoClose={5000}
+                    hideProgressBar={false}
+                    newestOnTop={false}
+                    closeOnClick
+                    rtl={false}
+                    pauseOnFocusLoss
+                    draggable
+                    pauseOnHover
+                    theme="light" />
             </div>
-            </div>
 
-            
+
         </div>
     );
 };
